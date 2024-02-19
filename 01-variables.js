acd2db4990183f0440ca25b1e03e3f421c34d527
PatiMarjim <pati.marjim@gmail.com>
2024-02-19 17:06:48 +0100

    create variables sesion 01

---
+++ 01-variables.js
@@ -0,0 +1,33 @@
+//"hola, que tal"
+//'hola, que tal'
+//"hi don't do it"
+//String
+var greetings = 'hola desde'
+var school = 'pmba'
+var greetingsFromSchool = greetings + ' ' + school
+
+//finalText
+//finalText
+//finalText (camelCase) = holaQueTal //forma principal y mayoritaria para utilizar variables en js//
+//FINAL_TEXT (PASCALCASE)
+//Valores que nunca van a cambiar,muy estáticos, no lo vamos a usar casi, es en React//
+console.log(greetingsFromSchool);
+
+//TIPOS DE VARIABLES: la forma que tiene la info que yo estoy almacenando. 
+/* String cadena de texto con " '"
+Number
+Boolean concepto verdaero o falso , 0 y 1, encendido o apagado
+Array colección de valores
+Object  librería de valores - clave valor*/
+
+var ageAsString = '30';//age, nada de comas, puede convertirse, pero puede haber errores
+var ageAsNumber = 30; //number/ los punto y comda o semicolon al final de línea son recomendados
+
+true //1 
+false //0
+
+var arrayOfValues = [30, '30', false, true, 4]
+var objectsOfValues = {
+    age: 30,
+    ageAsNumber: ageAsNumber
+}
