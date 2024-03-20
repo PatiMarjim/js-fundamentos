b98614d15bc1fb02b12c266db44304bb2516ef6a
PatiMarjim <pati.marjim@gmail.com>
2024-03-20 17:25:56 +0100

    13-variables-2.js

---
+++ 13-variables-2.js
@@ -0,0 +1,59 @@
+// var myName = 'Pati';
+// // myName = 'Jorge';
+
+// function changeMyName (newName) {
+//     nyName = newName;
+// // esto no se debería hacer
+// }
+
+// function sayMyName () {
+//     console.log(`Mi nombre es ${myName}`);
+// }
+
+// changeMyName ('Jorge');
+// sayMyName ();
+// // suele ser un oroblea, funciones globakes que cambian a raiz de lo que hace otra funcion, es perder control sobre el codigo, sobre lo que está pasando, para ello están las constantes, variables que no pueden reasginarse.
+
+// const myName = 'Pati';
+// myName = 'Pati';
+
+// // Utilizar siempre const, a menos que queramos reasginarlo, si queremos hacer eso en vez de crear const o var, crearemos let, es como var.
+
+// let myName = 'Pati';
+
+
+// function getNameInUpperCase(newName) {
+//     return newName.toUpperCase();
+// }
+
+// function sayMyName (name) {
+//     var name = 
+//     console.log (`mi nombre es ${name}`);
+// }
+
+// myName = getNameInUpperCase ('Jorge');
+// sayMyName (myName);
+
+// const school = 'PMBA';
+// let myName = 'Pati';
+
+// function getNameInUpperCase(newName) {
+//     return newName.toUpperCase();
+// }
+
+// function sayMyName (name) {
+
+//     console.log (`mi nombre es ${name}`);
+// }
+// function getStudies (name, school) {
+//     console.log(`Mi nombre es ${name} y estudio en ${school}`);
+// }
+
+// myName = getNameInUpperCase ('Jorge');
+// sayMyName (myName);
+
+// let y const se comportan igual en scooping, no afectan, a  diferencia de var. 
+// const no se puede aplicar en for, porque const no es reasinagble y en for se reasigna a variable siempre a cada vuelta.
+// let => La variable creada es reasignable e intenta no ser global
+
+// const => La variable creada NO es reasignable e intenta no ser global
